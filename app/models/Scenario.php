30fc5f76490a9f81b551ab35b072a78a029972e7
juanfrank2 <juanfrank2@gmail.com>
2014-05-02 22:02:56 +0200

    Se añade migración para case

--- app/models/Scenario.php
+++ app/models/Scenario.php
@@ -8,12 +8,12 @@ class Scenario extends Eloquent {
 		'description' => 'required'
 	);
 
-    public function grupos()
+    public function groups()
     {
         return $this->belongsToMany('Group', 'group_scenario', 'id_group', 'id_scenario');
     }
 
-    public function verbos()
+    public function steps()
     {
         return $this->belongsToMany('Step', 'scenario_step', 'id_scenario', 'id_step', 'order'); //revisar order
     }
